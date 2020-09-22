91d2bdf8715af95bd6eb341cace57be9a8a93a18
whitecanze <whitecanze123@gmail.com>
2020-09-22 18:53:17 +0700

    Update

--- pages/codeMirror.js
+++ pages/codeMirror.js
@@ -1,6 +1,21 @@
-import React from 'react'
-
+import React,{useEffect,useContext,useState} from 'react'
+import { withRouter } from 'next/router'
+import Router from 'next/router'
+import { AuthContext } from '../appState/AuthProvider'
 const Codemirror = () => {
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        if (!user) {
+            Router.push('/signin')
+        }
+        if (router.pathname === '/codeMirror') {
+            if ($('a').hasClass('nav-link')) {
+                $('a').removeClass('active')
+                $('#CodeMirror-btn').addClass('active')
+            }
+        }
+    })
     return (
         <div>
             CodeMirror
@@ -8,4 +23,4 @@ const Codemirror = () => {
     )
 }
 
-export default Codemirror
+export default withRouter(Codemirror)
